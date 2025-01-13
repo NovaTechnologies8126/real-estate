8088c9bace132335a9bd60931b234eb42c2492bc
Shredda8126 <novatechnologies8126@gmail.com>
2025-01-13 22:28:22 +0200

    Retry

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -8,7 +8,9 @@ export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
 
   if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local'
+    );
   }
 
   // Create new Svix instance with secret
--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     phoneNumber: {
-        type: number,
+        type: string,
       required: true,
     },
     profilePicture: {
